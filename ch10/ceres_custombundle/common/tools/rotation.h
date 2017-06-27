7bfc0801346094aca20c25ed48e0b26fb0bf7150
Chao Qu <quchao@seas.upenn.edu>
2017-06-27 15:58:16 -0400

    update

--- ch10/ceres_custombundle/common/tools/rotation.h
+++ ch10/ceres_custombundle/common/tools/rotation.h
@@ -6,45 +6,41 @@
 #include <limits>
 
 //////////////////////////////////////////////////////////////////
-// math functions needed for rotation conversion. 
+// math functions needed for rotation conversion.
 
-// dot and cross production 
+// dot and cross production
 
-template<typename T> 
+template <typename T>
 inline T DotProduct(const T x[3], const T y[3]) {
   return (x[0] * y[0] + x[1] * y[1] + x[2] * y[2]);
 }
 
-template<typename T>
-inline void CrossProduct(const T x[3], const T y[3], T result[3]){
+template <typename T>
+inline void CrossProduct(const T x[3], const T y[3], T result[3]) {
   result[0] = x[1] * y[2] - x[2] * y[1];
   result[1] = x[2] * y[0] - x[0] * y[2];
   result[2] = x[0] * y[1] - x[1] * y[0];
 }
 
-
 //////////////////////////////////////////////////////////////////
 
-
-// Converts from a angle anxis to quaternion : 
-template<typename T>
-inline void AngleAxisToQuaternion(const T* angle_axis, T* quaternion){
+// Converts from a angle anxis to quaternion :
+template <typename T>
+inline void AngleAxisToQuaternion(const T* angle_axis, T* quaternion) {
   const T& a0 = angle_axis[0];
   const T& a1 = angle_axis[1];
   const T& a2 = angle_axis[2];
   const T theta_squared = a0 * a0 + a1 * a1 + a2 * a2;
-  
-  
-  if(theta_squared > T(std::numeric_limits<double>::epsilon()) ){
+
+  if (theta_squared > T(std::numeric_limits<double>::epsilon())) {
     const T theta = sqrt(theta_squared);
     const T half_theta = theta * T(0.5);
-    const T k = sin(half_theta)/theta;
+    const T k = sin(half_theta) / theta;
     quaternion[0] = cos(half_theta);
     quaternion[1] = a0 * k;
     quaternion[2] = a1 * k;
     quaternion[3] = a2 * k;
-  }
-  else{ // in case if theta_squared is zero
+  } else {  // in case if theta_squared is zero
     const T k(0.5);
     quaternion[0] = T(1.0);
     quaternion[1] = a0 * k;
@@ -53,49 +49,46 @@ inline void AngleAxisToQuaternion(const T* angle_axis, T* quaternion){
   }
 }
 
-
-template<typename T>
-inline void QuaternionToAngleAxis(const T* quaternion, T* angle_axis){
+template <typename T>
+inline void QuaternionToAngleAxis(const T* quaternion, T* angle_axis) {
   const T& q1 = quaternion[1];
   const T& q2 = quaternion[2];
   const T& q3 = quaternion[3];
   const T sin_squared_theta = q1 * q1 + q2 * q2 + q3 * q3;
-  
+
   // For quaternions representing non-zero rotation, the conversion
   // is numercially stable
-  if(sin_squared_theta > T(std::numeric_limits<double>::epsilon()) ){
+  if (sin_squared_theta > T(std::numeric_limits<double>::epsilon())) {
     const T sin_theta = sqrt(sin_squared_theta);
     const T& cos_theta = quaternion[0];
-    
+
     // If cos_theta is negative, theta is greater than pi/2, which
     // means that angle for the angle_axis vector which is 2 * theta
     // would be greater than pi...
-    
-    const T two_theta = T(2.0) * ((cos_theta < 0.0)
-				  ? atan2(-sin_theta, -cos_theta)
-				  : atan2(sin_theta, cos_theta));
+
+    const T two_theta =
+        T(2.0) * ((cos_theta < 0.0) ? atan2(-sin_theta, -cos_theta)
+                                    : atan2(sin_theta, cos_theta));
     const T k = two_theta / sin_theta;
-    
+
     angle_axis[0] = q1 * k;
     angle_axis[1] = q2 * k;
     angle_axis[2] = q3 * k;
-  }
-  else{
+  } else {
     // For zero rotation, sqrt() will produce NaN in derivative since
-    // the argument is zero. By approximating with a Taylor series, 
-    // and truncating at one term, the value and first derivatives will be 
+    // the argument is zero. By approximating with a Taylor series,
+    // and truncating at one term, the value and first derivatives will be
     // computed correctly when Jets are used..
     const T k(2.0);
     angle_axis[0] = q1 * k;
     angle_axis[1] = q2 * k;
     angle_axis[2] = q3 * k;
   }
-  
 }
 
-
-template<typename T>
-inline void AngleAxisRotatePoint(const T angle_axis[3], const T pt[3], T result[3]) {
+template <typename T>
+inline void AngleAxisRotatePoint(const T angle_axis[3], const T pt[3],
+                                 T result[3]) {
   const T theta2 = DotProduct(angle_axis, angle_axis);
   if (theta2 > T(std::numeric_limits<double>::epsilon())) {
     // Away from zero, use the rodriguez formula
@@ -113,9 +106,9 @@ inline void AngleAxisRotatePoint(const T angle_axis[3], const T pt[3], T result[
     const T sintheta = sin(theta);
     const T theta_inverse = 1.0 / theta;
 
-    const T w[3] = { angle_axis[0] * theta_inverse,
-                     angle_axis[1] * theta_inverse,
-                     angle_axis[2] * theta_inverse };
+    const T w[3] = {angle_axis[0] * theta_inverse,
+                    angle_axis[1] * theta_inverse,
+                    angle_axis[2] * theta_inverse};
 
     // Explicitly inlined evaluation of the cross product for
     // performance reasons.
@@ -123,8 +116,7 @@ inline void AngleAxisRotatePoint(const T angle_axis[3], const T pt[3], T result[
                               w[2] * pt[0] - w[0] * pt[2],
                               w[0] * pt[1] - w[1] * pt[0] };*/
     T w_cross_pt[3];
-    CrossProduct(w, pt, w_cross_pt);                          
-
+    CrossProduct(w, pt, w_cross_pt);
 
     const T tmp = DotProduct(w, pt) * (T(1.0) - costheta);
     //    (w[0] * pt[0] + w[1] * pt[1] + w[2] * pt[2]) * (T(1.0) - costheta);
@@ -154,7 +146,7 @@ inline void AngleAxisRotatePoint(const T angle_axis[3], const T pt[3], T result[
                               angle_axis[2] * pt[0] - angle_axis[0] * pt[2],
                               angle_axis[0] * pt[1] - angle_axis[1] * pt[0] };*/
     T w_cross_pt[3];
-    CrossProduct(angle_axis, pt, w_cross_pt); 
+    CrossProduct(angle_axis, pt, w_cross_pt);
 
     result[0] = pt[0] + w_cross_pt[0];
     result[1] = pt[1] + w_cross_pt[1];
@@ -162,4 +154,4 @@ inline void AngleAxisRotatePoint(const T angle_axis[3], const T pt[3], T result[
   }
 }
 
-#endif // rotation.h
+#endif  // rotation.h
